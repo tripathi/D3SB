a487dfd6eceb9a85a69cd1deab6a4894b9a163d7
Anjali Tripathi <atripathi@cfa.harvard.edu>
2015-07-02 18:02:27 -0400

    Includes (commented out) capability for monotonic prior

--- lnprob.py
+++ lnprob.py
@@ -1,10 +1,19 @@
 import numpy as np
 from d3sbModel import d3sbModel
+import pdb
+import matplotlib.pyplot as plt
 
 def lnprob(theta, data, bins):
 
     if (theta<-20).any() or (theta>20).any():
         return -np.inf
+
+    if (theta<0).any():
+        return -np.inf
+
+##     if not (np.allclose(theta, np.sort(theta)[::-1])):
+## #        print 'Not monotonic'
+##         return -np.inf
 
     u, v, dreal, dimag, dwgt = data
     uvsamples = u, v
@@ -12,7 +21,7 @@ def lnprob(theta, data, bins):
     mreal = d3sbModel(theta, uvsamples, bins)
     mimag = np.zeros_like(u)
 
+
     chi2 = np.sum(dwgt*(dreal-mreal)**2) + np.sum(dwgt*(dimag-mimag)**2)
     lnp = -0.5*chi2
-
     return lnp
