a501664740b9fdd984aca50ee4825b34689a22ff
Anjali Tripathi <atripathi@cfa.harvard.edu>
2015-09-30 17:44:45 -0400

    Minor changes & housekeeping

--- gpprior.py
+++ gpprior.py
@@ -1,14 +1,15 @@
 import numpy as np
 import pdb
 import matplotlib.pyplot as plt
+from scipy.linalg import cho_factor, cho_solve
 
 #Calculate covariance matrix, given bin locations
 #Compute prior: [Atranspose Cinverse A] for a given set of weights
 
-def kexp2(i1, i2, bins=None, a=None, l=None):
-    ri = bins[i1]
-    rj = bins[i2]
-    return a*a * np.exp(-(ri - rj)**2./(2*l*l))
+def kexp2(i1, i2, ibins=None, a=None, l=None):
+    ri = ibins[i1]
+    rj = ibins[i2]
+    return a*a * np.exp(-((ri - rj)**2.)/(2.*l*l))
 
 def calccovar(binsin, ain, lin):
     '''
@@ -17,7 +18,7 @@ def calccovar(binsin, ain, lin):
     l: correlation length
     '''
     nbins = binsin.shape[0]
-    cov = np.fromfunction(kexp2,(nbins,nbins), bins=binsin, a=ain, l=lin, dtype = np.int)
+    cov = np.fromfunction(kexp2,(nbins,nbins), ibins=binsin, a=ain, l=lin, dtype = np.int)
     return cov
 
 def calcprior(weights, bins, a, l):
@@ -32,44 +33,49 @@ def calcprior(weights, bins, a, l):
     cb = 0.5*(cba+b)
 
     C = calccovar(cb, a, l)
-
+    #print C
+    # print 'Hi'
+    # plt.imshow(C, interpolation='nearest', cmap='Blues', origin = 'upper')
+    # plt.colorbar()
+    # plt.savefig('lv'+str(np.around(l, decimals = 2))+'.png')
+    # # plt.savefig('tmp'+str(np.around(clogdet, decimals=1))+'.png')
+    # plt.clf()
+    # print 'Bye'
 
     #Cholesky factorization, to invert it
-    #cholfac = np.linalg.cholesky(C)
-    #Cholesky solve to get first product
-    #Product on the other side
+    chofac, flag = cho_factor(C)
 
-    Cinv = np.linalg.inv(C)
+    #Find log det
+    clogdet = np.sum(2 * np.log((np.diag(chofac))))
 
-    prior = np.dot(np.dot(weights,Cinv), weights) #Check that this is correct
-    (sign,logdet) = np.linalg.slogdet(C)
+    #Non-Cholesky method
+    # Cinv = np.linalg.inv(C)
+    # prior = np.dot(np.dot(weights,Cinv), weights) #Check that this is correct
+    # (sign,logdet) = np.linalg.slogdet(C)
+
+
     # maxneg = 250.
     #if (sign < 0):
     #     if (logdet > maxneg):
     #         logdet = maxneg
     #     else:
+    #    print logdet, l
         #print "Warning - The determinant is negative, so something has gone wrong"
-    #print sign, logdet
 
-    # plt.imshow(C, interpolation='nearest', cmap='Blues')
-    # plt.colorbar()
-    # plt.savefig('tmp'+str(np.around(logdet, decimals=1))+'.png')
-    # plt.clf()
+
 #    result = prior + log stuff
 #RtC-1R _ ln det C + N ln 2pi
-    result = prior + logdet + cb.shape[0]*np.log(2.*np.pi)
+    #result = prior + logdet
+    #+ cb.shape[0]*np.log(2.*np.pi)
 
-    return result
+    #result =
+    return (np.dot(weights, cho_solve((chofac, flag), weights)) + clogdet)
 
 def priortest():
-    b = np.linspace(0.01, 0.5, num = 20)
+    b = np.linspace(0.01, 0.5, num = 6)
     weights = 1-b**2.
-    weights[1], weights[4] = weights[4], weights[1]
-    plt.plot(b, weights)
-    plt.show()
-    P = calcprior(weights, b, 1, b[2]-b[1])
+    binsa = [b[0], b]
+#    weights[1], weights[4] = weights[4], weights[1]
+    P = calcprior(weights, binsa, 1., 1.)
     print P
-
-
-
     return
