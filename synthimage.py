a354a333fc2f99eb7838d96eced892e16038d10f
Anjali Tripathi <atripathi@cfa.harvard.edu>
2015-12-15 13:13:55 -0500

    Used for preinference. Could be streamlined into an existing function

--- synthimage.py
+++ synthimage.py
@@ -80,3 +80,40 @@ def synthguess(a, b, nbins, filename):
     ## plt.show(block='False')
 
     return SBdscp
+
+def sb1d(filename):
+
+    # -- SYNTHESIZED IMAGE
+    # read in image, grab pixel dimensions in arcseconds
+    pimage, hdr = fits.getdata('DATA/'+filename, header=True)
+    pimage = np.squeeze(pimage)
+    nx  = hdr['NAXIS1']
+    ny  = hdr['NAXIS2']
+    xps = np.absolute(hdr['CDELT1']*3600.) #angular width of the pixels in the RA and DEC dimensions
+    yps = np.absolute(hdr['CDELT2']*3600.)
+
+    bmaj =  hdr['BMAJ']*3600
+    bmin =  hdr['BMIN']*3600
+
+    # construct the radial grid for the image (generic for projected viewing geometry)
+    incl = 0.
+    PA   = 0.
+    xp_,yp_ = np.meshgrid(xps*(np.arange(nx)-(nx/2.-0.5)),yps*(np.arange(ny)-(ny/2.-0.5)))
+    ang = np.radians(270.-PA)
+    xp  = np.cos(ang)*xp_-np.sin(ang)*yp_
+    yp  = np.sin(ang)*xp_+np.cos(ang)*yp_
+    rim = np.sqrt(xp*xp+1./np.cos(np.radians(incl))**2*yp*yp) #Radius
+    azang = np.arctan2(yp/np.cos(np.radians(incl)), xp) #Polar angle
+    azang[azang<0] = azang[azang<0]+2.*np.pi
+
+    # scale surface brightnesses to Jy/arcsec^2 units
+
+    ##    pimage /= xps*yps     #Originally used pixel size
+    pimage /= np.pi * bmaj * bmin/ (4. * np.log(2))
+
+    # flatten into a profile
+    SBpim = pimage.flatten()
+    rpim  = rim.flatten()
+    azangim = azang.flatten()
+
+    return (rpim, SBpim)
