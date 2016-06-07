e23d3015e9d02b8aecb5aabf926be334ca213f38
Anjali Tripathi <atripathi@cfa.harvard.edu>
2016-06-07 14:31:39 -0400

    1st working version of MCMC for hyperparams

--- analyticemcee.py
+++ analyticemcee.py
@@ -1,17 +1,43 @@
-def runemcee(p0, nsteps, nthreads, savename, dv, dw, fitproj=1):
+import numpy as np
+import pdb
+import matplotlib.pyplot as plt
+import scipy.special as sc
+import d3sbfxns as f
+import time
+from datetime import datetime
+import emcee
+
+def readinVis(datafile):
+    """
+    Extracts visibility data from a CASA and python generated .npz file.
+    :param datafile: name of .npz file
+    """
+    datfile = np.load(datafile)
+    return datfile['u'], datfile['v'], datfile['Vis'], datfile['Wgt']#, datfile['freq']  
+
+
+def initgpa(nwalkers):
+    #This is specific to GP amplitude, not generic parameters
+    return np.random.uniform(0,1,nwalkers).reshape((nwalkers,1))
+
+def runemcee(pin, nsteps, nthreads, savename, meanw):
     """
     Run emcee
-    :param p0: Initial walker positions
+    :param pin: Parameters
     :param nsteps: Number of MCMC steps to take
     :param savename: Output file name prefix
+    :param meanw: Mean to use for covariance
     """
     #Number of parameters and walkers, set by p0
-    ndim = np.shape(p0)[1]
-    nwalkers = ndim*4
+    ndim = 1#np.shape(pin)[1]
+    nwalkers = 16#ndim * 4
+    
+    #Initialize GP param walkers
+    p0 = initgpa(nwalkers)
+    
+    #Setup sampler
+    sampler = emcee.EnsembleSampler(nwalkers, ndim, lnprob, threads=nthreads, args=[meanw])
 
-
-    sampler = emcee.EnsembleSampler(nwalkers, ndim, lnprob, threads=nthreads, args=[dv, dw])
-       
     #Write log file
     f = open('analyticemcee.log', 'a')
     FORMAT = '%m-%d-%Y-%H%M'
@@ -28,25 +54,23 @@ def runemcee(p0, nsteps, nthreads, savename, dv, dw, fitproj=1):
     f.write(' ,'+str(round((toc-tic)/60., 2))+', '+str(np.round(np.mean(sampler.acceptance_fraction),2))+'\n')
     f.close()
 
-
     #Save the results in a binary file
-    np.save('mc'+str(fitproj)+'_'+savename,sampler.chain)
+    np.save('mc_'+savename,sampler.chain)
 
     print 'Done with this emcee run'
 
     return sampler.chain
 
 #@profile
-def lnprob(theta, dvis, dwgt):
+def lnprob(theta, meanw):
     """
     Compute log posterior for current parameter values
-    :param theta: Inclination params and bin coefficients (Inclination params must be put first, before the bins)
-    :param dvis: Complex visibilities
-    :param dwgt: Visibility weights, currently only real
+    :param theta: Hyperparameters to be optimized
+    :param meanw: Mean to use for covariance 
     """  
     # unpack the parameters
     gpa = theta[0]
-    gpl = theta[1]
+    #    gpl = theta[1]
 
  
     #PRIORS    
@@ -54,27 +78,32 @@ def lnprob(theta, dvis, dwgt):
     if (gpa<0).any():
         return -np.inf
 
-    if (gpl<0).any():
-        return -np.inf
+    ## if (gpl<0).any():
+    ##     return -np.inf
     
-
     #LIKELIHOOD
-    wcalc = analyticvals
-    mvis = np.dot(xi, wcalc)
+    wcalc = calcwtilde(gpa, meanw)
+    mvis = np.dot(x, wcalc)
 
     
     #Compute a chi2 value (proportional to log-likelihood)
     #Fix the imaginary component
-    chi2 = np.sum( dwgt*(dvis.real-mvis.real)**2 #+ dwgt*(dvis.imag-mvis.imag)**2 )
+    chi2 = np.sum( dwgt*(dvis.real-mvis.real)**2) #+ dwgt*(dvis.imag-mvis.imag)**2 )
 
 
     # return a log-posterior value
     return -0.5*(chi2)
 
 
-def analyticmean():
+def analyticmain():
+
+    #################
+    # 1. DATA SETUP #
+    #################
     #Data input
     hiresvis = 'DATA/fullA.vis.npz'
+    global dvis
+    global dwgt
     u, v, dvis, dwgt = readinVis(hiresvis)
     synthimg = 'DATA/fullA.image.fits'
     
@@ -83,10 +112,9 @@ def analyticmean():
     PA = 70.
     offx = -0.3
     offy = -0.2
-
     
     #Assign data vec and matrix
-    y = dvis.real[:, np.newaxis]
+    y = dvis.real[:, np.newaxis] #THIS IS ONLY REALS RIGHT NOW!
     sigma = np.diag(1./dwgt.real)
 
     #Compute rho   
@@ -104,6 +132,10 @@ def analyticmean():
 
     ## phase center shift
     shift = np.exp(-2.*np.pi*1.0j*((u*-offrx) + (v*-offry)))
+
+    #################
+    # 2. BINS SETUP #
+    #################
     
     #For now, extract bins from a file
     initfile = np.load('init_fullA_22.npz')
@@ -114,90 +146,106 @@ def analyticmean():
     binmin = 0.1
     binmax = 1.1
     rin = .01/140.
+    nbins = 20
+    b = np.linspace(binmin, binmax, num=nbins-1) 
+    rleft = np.insert(b[:-1],0,rin)
+    rright = b
+    N = rright.size
+    global cb
+    cb = 0.5*(rleft + rright)
 
-    ncycles = 1
+    #################
+    # 3. SET MEAN W #
+    #################
     
-    for count in range(ncycles):
-        tic = time.time()
-        nbins = 20#(count+1)*20
-        b = np.linspace(binmin, binmax, num=nbins-1) 
+    #Find mean & std.dev. values at bin locations from synthesized image
+    #    rsb, sb, beaminfo = f.sbdata(synthimg, PAr, inclr, offrx, offry) #Add plotting argument to see image, if desired
+    #sbbin, sigmabin = f.sbmeanbin(rin, b, rsb, sb)
 
-        #Find mean & std.dev. values at bin locations from synthesized image
-        rsb, sb, beaminfo = f.sbdata(synthimg, PAr, inclr, offrx, offry) #Add plotting argument to see image, if desired
-        sbbin, sigmabin = f.sbmeanbin(rin, b, rsb, sb)
+
+    #Calculate truth before exiting
+    flux = 0.12
+    sig = 0.6
+    incl = 50.
+    PA = 70.
+    offx = -0.3
+    offy = -0.2
+    nominal_SB = (sig/cb)**0.7 * np.exp(-(cb/sig)**2.5)	# fullA distribution; where
+    # flux=0.12, sig=0.6, i=50, 
+    # PA = 70., offs=[-0.3, -0.2]
+    int_SB = np.trapz(2.*np.pi*nominal_SB*cb, cb)		# a check on the total flux
+    nominal_SB *= flux / int_SB
     
-            
-        rleft = np.insert(b[:-1],0,rin)
-        rright = b
-        cb = 0.5*(rleft + rright)
-    
-        N = rright.size
-    
-
-
-    #For ease of quick implementation, I'm coding up the function on paper, not the simplification Sean uses
-    ## jarg = np.outer(2.*np.pi*rbin, rho)
-    #Failed attempt at doing this with vectors
-    #    jargleft = np.outer(rho, 2.*np.pi*rleft)
-    #    jargright = np.outer(rho, 2.*np.pi*rright)
-    #    jright = sc.j1(jargright)
-    #    jleft = sc.j1(jargleft)
-    ## jinc = sc.j1(jarg)/jarg
-    ## vrealnoshift = np.dot(2.*np.pi*rbin**2*intensity, jinc)
-    #    x = (rright*sc.j1(jargright) - rleft*sc.j1(jargleft))
-
-    #sigmainv = np.diag(dwgt.real) #Not used
+    #####################
+    # 4. ANALYTIC SETUP #
+    #####################
     
     #Calculate x, V, and w_hat
-        x = np.empty([M,N])
-        intermed = np.empty([M,N])
-        rhsright = np.empty(M)
-        for j in range(M):
-            for i in range(N): 
-                x[j][i] = 1./rho[j]*(rright[i]*sc.j1(rho[j]*2.*np.pi*rright[i]) - rleft[i]*sc.j1(rho[j]*2.*np.pi*rleft[i]))
-                intermed[j][i] = dwgt.real[j]*x[j][i]
-                rhsright[j] = dwgt.real[j]*y[j]
+    global x
+    x = np.empty([M,N])
+    intermed = np.empty([M,N])
+    rhsright = np.empty(M)
+    for j in range(M):
+        for i in range(N): 
+            x[j][i] = 1./rho[j]*(rright[i]*sc.j1(rho[j]*2.*np.pi*rright[i]) - rleft[i]*sc.j1(rho[j]*2.*np.pi*rleft[i]))
+            intermed[j][i] = dwgt.real[j]*x[j][i]
+            rhsright[j] = dwgt.real[j]*y[j]
+    global lhs
+    lhs = np.linalg.inv(np.dot(np.transpose(x),intermed))  #This is Vw undertilde inverse
+    rhs = np.dot(np.transpose(x), rhsright)
+    global result
+    result = np.dot(lhs, rhs) #This is w_hat
+    
+    ################
+    # 5. RUN EMCEE #
+    ################
 
-        lhs = np.linalg.inv(np.dot(np.transpose(x),intermed))  #This is Vw undertilde inverse
-        rhs = np.dot(np.transpose(x), rhsright)
-        result = np.dot(lhs, rhs) #This is w_hat
-        
-        #Calculate truth before exiting
-        flux = 0.12
-        sig = 0.6
-        incl = 50.
-        PA = 70.
-        offx = -0.3
-        offy = -0.2
-        nominal_SB = (sig/cb)**0.7 * np.exp(-(cb/sig)**2.5)	# fullA distribution; where
-        # flux=0.12, sig=0.6, i=50, 
-        # PA = 70., offs=[-0.3, -0.2]
-        int_SB = np.trapz(2.*np.pi*nominal_SB*cb, cb)		# a check on the total flux
-        nominal_SB *= flux / int_SB
-        
-        np.savez('linalgparts', what = result, x=x, y = y, sigmainvdiag = dwgt.real, v = lhs, truth = nominal_SB, bincenter = cb)
-        
-        #############
-        #With  prior#
-        #############
-        
-        #Hyperparameter guesses
-        gpa = 1.
-        gam = count*(-.5)
-        gpl = cb[3]-cb[1]
-        C = calccovar(cb, gpa, gpl, gam)
-        Cinv = np.linalg.inv(C)
-        Swinv = lhs + Cinv
+    #
+    gpaguess = np.array([0.05])#np.random.uniform(0,1,nwalkers)
+    nsteps = 10000
+    nthreads = 2
+    notes = raw_input('Notes? Only give a short phrase to append to filenames\n')
+    savename = notes+'_'+str(nbins)
 
-        checkprod = np.dot (C, Cinv)
-        
-        intermed2 = np.dot(Cinv,nominal_SB)
-        
-        wtilde = np.dot(np.linalg.inv(Swinv),np.dot(lhs,result)) + np.dot(np.linalg.inv(Swinv),intermed2)
-        toc = time.time()
-        print 'Time taken is ', toc-tic
-        print 'Wtilde is:', wtilde    
+    print 'Going to start emcee'
+    chain1 = runemcee(gpaguess, nsteps, nthreads, savename,nominal_SB)
 
-#def calcwtilde():
+    pdb.set_trace()
+    
+    #        np.savez('linalgparts', what = result, x=x, y = y, sigmainvdiag = dwgt.real, v = lhs, truth = nominal_SB, bincenter = cb)
+        
 
-#Make x global        
+def calcwtilde(gpa, meanw):        
+    #Use GP prior
+
+    #Hyperparameters
+    #    gpa = 1.
+    gam = 1.
+    gpl = cb[3]-cb[1]
+
+    #Calculate covariance
+    C = calccovar(cb, gpa, gpl, gam)
+    Cinv = np.linalg.inv(C)
+    Swinv = lhs + Cinv
+
+    #Return new mean (wtilde)
+    intermed2 = np.dot(Cinv,meanw)
+    return np.dot(np.linalg.inv(Swinv),np.dot(lhs,result)) + np.dot(np.linalg.inv(Swinv),intermed2)
+
+def kexp2(i1, i2, ibins=None, a=None, l=None, gamma=None):
+    ri = ibins[i1]
+    rj = ibins[i2]
+    return ri**gamma*a*a * np.exp(-((ri - rj)**2.)/(2.*l*l)) #Doesn't have cross term
+
+def calccovar(binsin, ain, lin, gamin):
+    '''
+    Calculate the covariance matrix using a squared exponential kernel
+    a: normalization amplitude
+    l: correlation length
+    '''
+    nbins = binsin.shape[0]
+    cov = np.fromfunction(kexp2,(nbins,nbins), ibins=binsin, a=ain, l=lin, gamma = gamin, dtype = np.int)
+    return cov
+
+if __name__ == "__main__":
+    analyticmain()
