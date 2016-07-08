40d7b77bf5cae9299fa70c44c04315eeead5ecd2
Anjali Tripathi <atripathi@cfa.harvard.edu>
2016-07-08 00:33:36 -0400

    First version with uniform prior results. Still unsure about real/imag

--- linearsoln.py
+++ linearsoln.py
@@ -1,4 +1,5 @@
 import numpy as np
+import scipy.special as sc
 import matplotlib.pyplot as plt
 import pdb
 from deprojectVis import deproject_vis
@@ -33,31 +34,37 @@ def main():
         rhodeproj, Ddeproj, sigdeproj = deproject_vis([uorig, vorig, Dorig, Dwgt], incl=incl, PA=PA, offx=offx, offy=offy)
     D = Ddeproj
     rho = rhodeproj
-    Sigmainv = 1./np.square(np.absolute(sigdeproj))
+    Sigmainv = np.diag(1./(np.square(sigdeproj.real) + np.square(sigdeproj.imag)*1j))
+    #np.absolute(sigdeproj))))
+    #
     Ndata = D.size
     print 'Number of vis is', Ndata, np.shape(rho), np.shape(Sigmainv)
 
     #We have a model visibility (M) which uses Nbins annuli
-    #M=Xw, where X is only a function of the bins and baseline
 
     #Select model annuli radii
     rmin = 0.01/140.
     rmax = 1.1
-    nbins = 30
-    radii = np.linspace(rmin, rmax, num=nbins+1) #Currently does NOT use rin
+    Nbins = 20
+    radii = np.linspace(rmin, rmax, num=Nbins+1) #Currently does NOT use rin
     rleft = radii[:-1]
     rright = radii[1:]
     rcenter = (rleft+rright)/2.
 
+    #M=Xw, where X is only a function of the bins and baselines
+    X = np.empty([Ndata,Nbins])
+    for j in range(Ndata):
+        for i in range(Nbins):
+            X[j][i] = 1./rho[j]*(rright[i]*sc.j1(rho[j]*2.*np.pi*rright[i]) - rleft[i]*sc.j1(rho[j]*2.*np.pi*rleft[i]))
+
+    #Calculate uniform prior mean and covariance matrix
+    #The mean of the distribution with a uniform prior is wu, with covariance Cu
+
+    Cu = np.dot(np.dot(X.T, Sigmainv), X)
+    Cuinv = np.linalg.inv(Cu)
+    wu = np.dot(Cuinv, np.dot(np.dot(X.T, Sigmainv), D))
+
     pdb.set_trace()
-# X =
-#
-# #Calculate uniform prior mean and covariance matrix
-# #The mean of the distribution with a uniform prior is wu, with covariance Cu
-#
-# Cu = np.dot(np.dot(X.T, Sigmainv), X)
-# Cuinv = np.linalg.inv(Cu)
-# wu = np.dot(Cuinv, np.dot(np.dot(x.T, Sigmainv), Ddeproj))
 #
 # #Calculate the GP covariance matrix (Cw) from the kernel (k), with mean muw
 # #The mean of the distribution with this prior is wgh, with variance Cgp
